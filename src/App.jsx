84a8da7f7401d3e4658a1059f2ec29c446ccadaf
Tshiamo Tsoku <ttsoku@icloud.com>
2024-03-17 05:51:52 -0700

    All paths created

--- src/App.jsx
+++ src/App.jsx
@@ -148,12 +148,12 @@ import './App.css'
 
 // export default App
 import React from 'react';
-import Footer from '../components/Footer';
-import ImageScrollbar from '../components/ImageScrollbar';
-import Layout from '../components/Layout';
-import Navbar from '../components/Navbar';
-import Characters from '../components/Characters';
-import SearchFilters from '../components/SearchFilters';
+import Footer from './components/Footer';
+import ImageScrollbar from './components/ImageScrollbar';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
+import Characters from './components/Characters';
+import SearchFilters from './components/SearchFilters';
 
 const App = () => {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>Footer content goes here</p>
+      <p> © 2021 Walt Disney, Inc.</p>
     </footer>
 
-const theFooter = () => (
-  <Box>
-    © 2021 Realtor, Inc.
-  </Box>
-);
   );
 };
 
